a57f822a05a8f1ddd0aacdeca6e6135738c83767
CancisioKarl <162144872+CancisioKarl@users.noreply.github.com>
2025-02-03 16:33:07 +0800

    login design

--- cab-frontend/src/components/Login.jsx
+++ cab-frontend/src/components/Login.jsx
@@ -1,25 +1,38 @@
 import React from "react";
-import {Button, Container, Form} from 'react-bootstrap';
+import { Button, Container, Form } from "react-bootstrap";
 
 const Login = () => {
-    return(
-        <Container>
-            <Form>
-                <Form.Label>LOGIN</Form.Label>
-                <Form.Group style={{alignContent: 'start', textAlign: 'start'}}>
-                    <Form.Label className="justify-content-start">Email address:</Form.Label>
-                    <Form.Control controlId='email'></Form.Control>
-                </Form.Group>
-                <Form.Group className="mt-5 mb-5    " style={{alignContent: 'start', textAlign: 'start'}}>
-                    <Form.Label className="justify-content-start">Password:</Form.Label>
-                    <Form.Control contolId='passwords'></Form.Control>
-                </Form.Group>
-                <Button type='submit'> Login </Button>
-                
-            </Form>
-            <a className='' href="">Register as an Evaluator</a>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="mt-5 loginDiv">
+        <Form>
+          <Form.Label className="cabWad">Cabuyao Water District</Form.Label>
+          <p className="ims">Inventory Management System</p>
+          <Form.Group style={{ alignContent: "start", textAlign: "start" }}>
+            <Form.Label className="justify-content-start text-white mt-4 h6">
+              Username:
+            </Form.Label>
+            <Form.Control className="login" controlId="email"></Form.Control>
+          </Form.Group>
+          <Form.Group
+            className="mt-5 mb-5"
+            style={{ alignContent: "start", textAlign: "start" }}
+          >
+            <Form.Label className="justify-content-start text-white h6">
+              Password:
+            </Form.Label>
+            <Form.Control
+              className="password"
+              contolId="passwords"
+            ></Form.Control>
+          </Form.Group>
+          <div className="loginbtn">
+            <Button variant="warning" className="text-white" style={{fontWeight: "bold", width: "10em"}} type="submit"> Login </Button>
+          </div>
+        </Form>
+      </div>
+    </Container>
+  );
 };
 
 export default Login;
