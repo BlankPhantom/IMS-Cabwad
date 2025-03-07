f88b71e6b631531a4d9513b3412ab113fc63530a
Laurence Andrew B. Santos <laurencesantos62@gmail.com>
2025-03-07 11:15:36 +0800

    Update views.py

--- cab_backend/backup/views.py
+++ cab_backend/backup/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from utils import backup_database, restore_database
+from .utils import backup_database, restore_database
 import os
 class BackupDatabaseView(APIView):
     def get(self, request):
