64e8f6b99178728901750e77d3ca07e627ad5c88
CancisioKarl <162144872+CancisioKarl@users.noreply.github.com>
2025-04-04 15:15:48 +0800

    last minute

--- cab-frontend/src/components/Button/BtnEditDeleteMaster.jsx
+++ cab-frontend/src/components/Button/BtnEditDeleteMaster.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { faClipboard, faPenToSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faClipboardList, faPenToSquare, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from 'react-bootstrap';
 
 const BtnEditDeleteMaster = ({ onEdit, onDelete, onViewHistory }) => {
     return (
         <div className="d-flex gap-2">
-            <Button variant="info" size="sm" style={{fontSize: '9px'}} onClick={onViewHistory}>
-                <FontAwesomeIcon icon={faClipboard} /> 
+            <Button variant="primary" size="sm" style={{fontSize: '9px'}} onClick={onViewHistory}>
+                <FontAwesomeIcon icon={faClipboardList} /> 
             </Button>
-            <Button variant="primary" size="sm" style={{fontSize: '9px'}} onClick={onEdit}>
+            <Button variant="warning" size="sm" style={{fontSize: '9px'}} onClick={onEdit}>
                 <FontAwesomeIcon icon={faPenToSquare} /> 
             </Button>
             <Button variant="danger" size="sm" style={{fontSize: '9px'}} onClick={onDelete}>
